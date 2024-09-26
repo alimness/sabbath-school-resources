5b5fab5d1034711c0c8e15870b42fc2001c87865
VitalikL <lim.vitaliy@gmail.com>
2024-09-26 15:13:31 -0400

    Implement credits for audio blocks

--- ops/helpers/blocks/audio.js
+++ ops/helpers/blocks/audio.js
@@ -6,11 +6,31 @@ export const audio = {
             const rule = /^!\s*(a|audio)\s*\[([^\]\n\[]+)\]( *[^\n]*)\n?/
             const match = rule.exec(src);
             if (match) {
+                let caption = match[3] ?? null
+                let credits = null
+                if (caption) {
+                    caption = caption.replace(/[\(\)]/g, "").trim()
+                    const creditsRule = /({\s*"credits"\s*:.*})/g
+                    const hasCredits = creditsRule.exec(caption)
+
+                    if (hasCredits) {
+                        try {
+                            credits = JSON.parse(hasCredits[1])
+                        } catch (e) {
+                            console.error(e)
+                        }
+
+                        caption = caption.replace(creditsRule, '').trim()
+                        caption = caption.length ? caption : null
+                    }
+                }
+
                 return {
                     type: "audio",
                     raw: match[0],
                     target: match[2].trim(),
-                    caption: match[3] ? match[3].replace(/[\(\)]/g, "").trim(): null,
+                    caption,
+                    credits,
                 };
             }
         },
@@ -20,6 +40,6 @@ export const audio = {
         }
     },
     process: async function (block) {
-        return { id: block.id, type: block.type, src: block.target, caption: block.caption }
+        return { id: block.id, type: block.type, src: block.target, caption: block.caption, credits: block.credits }
     },
 }
