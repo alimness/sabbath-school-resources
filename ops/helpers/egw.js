921aa152dfdf502138626c882a1636cbd11159ea
VitalikL <lim.vitaliy@gmail.com>
2024-10-16 14:10:37 -0400

    Make EGW to be conditional based on language info

--- ops/helpers/egw.js
+++ ops/helpers/egw.js
@@ -1,6 +1,18 @@
 import searchEGW from "@Adventech/egw-tools"
+import { getLanguageInfoSync } from "../deploy/deploy-languages.js"
+
+let languageInfoGlobal = {}
 
 export const getEGWData = async function (resourcePath, text) {
-    return await searchEGW.searchEGW(resourcePath.language, text)
+    if (!languageInfoGlobal[resourcePath.language]) {
+        const languageInfo = getLanguageInfoSync(resourcePath.language)
+        languageInfoGlobal[resourcePath.language] = languageInfo
+    }
+
+    if (languageInfoGlobal[resourcePath.language].egw) {
+        return await searchEGW.searchEGW(resourcePath.language, text)
+    }
+
+    return null
 }
 
