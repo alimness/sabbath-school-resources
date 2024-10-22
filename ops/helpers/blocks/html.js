2bfe80485374462c51b1f7416840109e4d56c7e3
VitalikL <lim.vitaliy@gmail.com>
2024-10-22 00:09:03 -0400

    Improve HTML processing

--- ops/helpers/blocks/html.js
+++ ops/helpers/blocks/html.js
@@ -1,10 +1,10 @@
 import { NodeHtmlMarkdown } from 'node-html-markdown'
-import { parseBlock, parseSegment } from "../blocks.js"
+import { parseSegment } from "../blocks.js"
 
 export const html = {
     extension: {},
     process: async function (block, resourcePath) {
-        if (/<(table|img|center)>?/g.test(block.raw)) {
+        if (/<(table|img|center|p|b|i)>?/g.test(block.raw)) {
             try {
                 let ret = (await parseSegment(NodeHtmlMarkdown.translate(block.raw), resourcePath))[0] ?? null
 
@@ -12,8 +12,6 @@ export const html = {
             } catch (e) {
                 console.info(`Error occurred parsing HTML table, skipping`, e)
             }
-
-            return false
         }
         return false
     },
